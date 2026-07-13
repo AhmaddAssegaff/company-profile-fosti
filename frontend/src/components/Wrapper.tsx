986b5ed29f42edd62fa2fbc5aa8524b2db4485e4
FahriSulaimannn <fahrisulaiman316@gmail.com>
2026-07-13 22:08:48 +0700

    feat: add Achievements section to showcase milestones

--- frontend/src/components/Wrapper.tsx
+++ frontend/src/components/Wrapper.tsx
@@ -4,12 +4,14 @@ import Stats from "./Stats";
 import Divisi from "./Divisi";
 import About from "./About";
 import Partners from "./Partners";
+import Achievements from "./Achievement";
 
 const Wrapper = () => {
   return (
     <div className="space-y-12">
       <Hero />
       <About />
+      <Achievements />
       <Divisi />
       <Partners />
     </div>
